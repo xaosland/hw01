ba70d587b08b838e586dde5bc292525fcb5b519f
xaosland <xaosland@mail.ru>
2023-12-11 20:03:12 +0400

    first commit

---
+++ src/s2-homeworks/hw01/friend-message/FriendMessage.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import s from './FriendMessage.module.css'
+import {MessageType} from "../HW1";
+
+type friendMessage = {
+    message: MessageType
+}
+
+const FriendMessage = (props: friendMessage) => {
+    return (
+        <div id={'hw1-friend-message-' + props.message.id} className={s.friendMessage}>
+            <div className={s.friendImageAndText}>
+                <img id={'hw1-friend-avatar-' + props.message.id} src={props.message.user.avatar}
+                     alt={props.message.user.name}/>
+                <div className={s.friendText}>
+                    <div id={'hw1-friend-name-' + props.message.id} className={s.friendName}>
+                        {props.message.user.name}
+                    </div>
+                    <pre id={'hw1-friend-text-' + props.message.id} className={s.friendMessageText}>
+                        {props.message.id}
+                    </pre>
+                </div>
+            </div>
+            <div id={'hw1-friend-time-' + props.message.id} className={s.friendTime}>
+                {props.message.message.time}
+            </div>
+        </div>
+    )
+}
+
+export default FriendMessage
